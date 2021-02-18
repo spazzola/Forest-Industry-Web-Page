58e8fead6ddc6c2993653830575e698c96284f6e
Rafal Szczotka <rafalszczotka@wp.pl>
2021-02-18 22:12:37 +0100

    Added Footer

---
+++ src/components/web-page/content/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <footer class="col the-footer">
+      &copy; 2020 Forest Industry S.A. All rights reserved.
+    </footer>
+  </div>
+</template>
+
+<style>
+.the-footer {
+  position: fixed !important;
+  bottom: 0;
+  font-family: "Libre Baskerville", serif;
+  text-align: center;
+  font-weight: bold;
+  width: 100%;
+  font-size: 0.9rem;
+  background: rgb(136, 158, 129);
+  padding: 0.7rem;
+}
+</style>
