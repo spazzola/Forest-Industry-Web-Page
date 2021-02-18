1a8195757f089559cf47fcb213441ba4e43e12b3
Rafal Szczotka <rafalszczotka@wp.pl>
2021-02-18 22:12:56 +0100

    Added Contact and AboutCompany

---
+++ src/components/web-page/content/AboutCompany.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav-menu></nav-menu>
+  <div>
+    <h1>O firmie</h1>
+    <div class="content">
+      Działamy na rynku leśnym od 1997 roku. Do 2020 roku pracując w Polsce
+      firma była znana pod nazwą Gospodarka Leśna S.A, obecnie jesteśmy
+      rozpoznawalni jako Forest Industry S.A. 
+      Przez cały ten czas nawiązaliśmy szereg różnorodnych kontaktów, dzięki
+      którym weszliśmy na rynek europejski zachowując zaufanie oraz dobrą opinię
+      klientów. <br />
+      <br />
+      Zajmujemy się wszelkimi pracami związanymi z gospodarką leśną, handlem
+      drewna oraz budową domów z bali na terenie Europy.<br />
+      Nasze składnice znajdują się głównie w Czechach oraz na Słowacji. <br />
+      Posiadamy certyfikaty PEFC oraz FSC&reg;.
+    </div>
+  </div>
+  <the-footer></the-footer>
+</template>
+
+<script>
+import NavMenu from "../nav/NavMenu.vue";
+import TheFooter from './TheFooter.vue';
+
+
+export default {
+  components: {
+    NavMenu,
+    TheFooter
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  margin-top: 3rem;
+  text-align: center;
+  font-family: 'Merriweather', serif;
+}
+
+.content {
+  text-align: center;
+  max-width: 50%;
+  margin: auto;
+  margin-top: 2rem;
+  line-height: 1.5;
+}
+
+@media (max-width: 1028px) {
+  .content {
+    max-width: 90%;
+  }
+}
+</style>
---
+++ src/components/web-page/content/TheContact.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav-menu></nav-menu>
+
+  <div class="content">
+    <div class="contactTile">
+      <h2>Prezes firmy</h2>
+      Adam Szczotka <br />
+      tel.: +48 602 597 988 <br />
+      biuro@forestindustrysa.pl <br />
+      <br />
+    </div>
+
+    <div class="contactTile">
+      <h2>Dyrektor handlowy</h2>
+      Dalibor Leher <br />
+      tel.: +48 797 860 707 <br />
+      tel.: +420 602 139 114 <br />
+      biuro@forestindustrysa.pl <br />
+      <br />
+    </div>
+
+    <div class="contactTile">
+      <h2>Technik administracji</h2>
+      Bartosz Wisiński <br />
+      tel.: +48 505 605 469 <br />
+      biuro@forestindustrysa.pl <br />
+      <br />
+    </div>
+    <div style="clear: both"></div>
+
+    <div class="contactTile">
+      <h2>Dane firmy</h2>
+      Rycerka Górna 283C<br />
+      34-370 Rajcza <br />
+      NIP: 553 100 62 39 <br />
+      IBAN: 18 1020 1390 0000 6702 0642 7589 <br />
+      SWIFT: BPKOPLPW <br />
+      VAT: 23 1020 1390 0000 6502 0642 7597<br />
+    </div>
+  </div>
+  <the-footer></the-footer>
+</template>
+
+<script>
+import NavMenu from "../nav/NavMenu.vue";
+import TheFooter from '../content/TheFooter.vue'
+
+export default {
+  components: {
+    NavMenu,
+    TheFooter
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  text-align: center;
+}
+
+.contactTile {
+  font-size: 1rem;
+  margin-top: 20px;
+}
+</style>
