8c4534917a4ff84e411854db9a85e92eea0b772a
Rafal Szczotka <rafalszczotka@wp.pl>
2021-02-18 22:14:38 +0100

    Added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import AboutCompany from '../components/web-page/content/AboutCompany.vue'
+import TheContact from '../components/web-page/content/TheContact.vue'
+import TheGallery from '../components/web-page/content/TheGallery.vue'
+import LogHomes from '../components/web-page/content/offer/LogHomes.vue'
+import WoodTrading from '../components/web-page/content/offer/WoodTrading.vue'
+
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/about', alias: '/',
+    name: 'AboutCompany',
+    component: AboutCompany
+  },
+  {
+    path: '/contact',
+    name: 'TheContact',
+    component: TheContact
+  },
+  {
+    path: '/gallery',
+    name: 'TheGallery',
+    component: TheGallery
+  },
+  {
+    path: '/offer/woodtrading',
+    name: WoodTrading,
+    component: WoodTrading
+  },
+  {
+    path: '/offer/loghomes',
+    name: LogHomes,
+    component: LogHomes
   }
 ]
 
